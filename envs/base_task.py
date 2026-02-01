31827595b602b815b4d555a075c3b91fe2a31bd7
Felix Loos <felix.loos@t-online.de>
2026-02-01 15:27:28 +0000

    added dribbeling training

--- envs/base_task.py
+++ envs/base_task.py
@@ -135,10 +135,18 @@ class BaseTask:
                 camera_props.use_collision_geometry = False
                 self.camera = self.gym.create_camera_sensor(self.envs[self.cfg["viewer"]["record_env_idx"]], camera_props)
                 self.camera_frames = []
+
+            if self.root_states[self.cfg["viewer"]["record_env_idx"]].ndim == 2:
+                # Multiple actors case
+                root_pos = self.root_states[self.cfg["viewer"]["record_env_idx"], 0, 0:3].tolist()
+            else:
+                # Single actor case
+                root_pos = self.root_states[self.cfg["viewer"]["record_env_idx"], 0:3].tolist()
+
             cam_pos = gymapi.Vec3(
-                *(x + y for x, y in zip(self.root_states[self.cfg["viewer"]["record_env_idx"], 0:3].tolist(), self.cfg["viewer"]["pos"]))
+                *(x + y for x, y in zip(root_pos, self.cfg["viewer"]["pos"]))
             )
-            cam_target = gymapi.Vec3(*self.root_states[self.cfg["viewer"]["record_env_idx"], 0:3].tolist())
+            cam_target = gymapi.Vec3(*root_pos)
             self.gym.set_camera_location(self.camera, self.envs[self.cfg["viewer"]["record_env_idx"]], cam_pos, cam_target)
             self.gym.render_all_camera_sensors(self.sim)
             img = self.gym.get_camera_image(self.sim, self.envs[self.cfg["viewer"]["record_env_idx"]], self.camera, gymapi.IMAGE_COLOR)
